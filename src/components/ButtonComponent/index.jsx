2e4269349d1f6fac5f2d979f7b5ade5839af681f
AidaFig <aidafigueiredo8@gmail.com>
2021-07-31 16:38:35 -0300

    click event added

--- src/components/ButtonComponent/index.jsx
+++ src/components/ButtonComponent/index.jsx
@@ -1,9 +1,11 @@
 import * as S from "./ButtonStyled";
 
 export const Button = ({ width, border, color, shadow, background, children }) => {
+  const Click = () => {
+  };
   return (
     <div>
-      <S.Button type="button" width={width} border={border} color={color} shadow={shadow} background={background}>{children}</S.Button>
+      <S.Button type="button" width={width} border={border} color={color} shadow={shadow} background={background} onClick={Click}>{children}</S.Button>
     </div>
   );
 };
