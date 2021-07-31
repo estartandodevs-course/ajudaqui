f2be0a2855fd4fbbd1b2f64ee2bf3bb8ba5e99cb
AidaFig <aidafigueiredo8@gmail.com>
2021-07-31 13:43:50 -0300

    feat: WIP

---
+++ src/components/ButtonComponent/index.jsx
@@ -0,0 +1,9 @@
+import * as S from "./ButtonStyled.js";
+
+export const Button = () => {
+  return (
+    <div>
+      <S.Button type="button">Cadastrar</S.Button>
+    </div>
+  );
+};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "../../components/ButtonComponent";
 
 const Home = () => {
-  return <div>home works</div>;
+  return;
+    <>
+      <div>home works</div>
+
+      <Button />
+    </>;
 };
 
 export default Home;
