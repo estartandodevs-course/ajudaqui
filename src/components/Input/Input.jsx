8da56db124e8d199be7ff960003d16e12939367d
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-07-31 00:11:03 -0300

    Feat: base structure created

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./InputStyles";
+
+export const Input = () => {
+  return (
+    <div>
+      <S.Title>Olá Mundo</S.Title>
+    </div>
+  );
+};
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Home } from "./pages";
+// import { Home } from "./pages";
+import Input from "./components/Input/index";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Input} />
       </Switch>
     </BrowserRouter>
   );
