aec394d9ac1c6c094d7fa8f197d4804d522004a1
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-31 21:55:19 -0300

    feat: import theme provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import Routes from "./routes";
 import { GlobalStyle } from "./styles/global";
+import { theme } from "./styles/themes";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
