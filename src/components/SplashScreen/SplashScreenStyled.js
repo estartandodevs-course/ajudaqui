d08e8c7669908eb7673bb4a81c3b175544a5de2c
AidaFig <aidafigueiredo8@gmail.com>
2021-08-02 14:33:14 -0300

    adding style to the SplashScreen component

--- src/components/SplashScreen/SplashScreenStyled.js
+++ src/components/SplashScreen/SplashScreenStyled.js
@@ -1,1 +1,27 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+export const ImageTop = styled.img`
+  position: static;
+`;
+
+export const ImageCenter = styled.img`
+  margin: 32px 0;
+  text-align: center;
+  max-width: 203px;
+  max-height: 203px;
+`;
+export const ImageSubLogo = styled.img`
+  margin-bottom: 43px;
+`;
+
+export const ImageBottom = styled.img`
+  margin: 0;
+  position: absolute;
+  bottom: 0;
+  left: 0px;
+`;
