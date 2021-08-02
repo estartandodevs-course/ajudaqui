dba12d1ad5f9a1b2c53f29e7122f7837c65fc3cc
AidaFig <aidafigueiredo8@gmail.com>
2021-08-02 15:51:58 -0300

    adding typography on the splash screen

--- src/components/SplashScreen/SplashScreenStyled.js
+++ src/components/SplashScreen/SplashScreenStyled.js
@@ -1,12 +1,33 @@
 import styled from "styled-components";
+import { Typography } from "../Typography";
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   flex-direction: column;
-`;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  `;
+export const Title = styled(Typography).attrs({
+  variant: "h2",
+  weight: 400,
+  color: "#4E3681",
+})`
+
+  `;
+export const Subtitle = styled(Typography).attrs({
+  variant: "body1",
+  weight: 400,
+  color: "#4E3681",
+})`
+
+    `;
 export const ImageTop = styled.img`
   position: static;
+  max-width: 100%;
+  top: 0;
 `;
 
 export const ImageCenter = styled.img`
@@ -20,8 +41,8 @@ export const ImageSubLogo = styled.img`
 `;
 
 export const ImageBottom = styled.img`
-  margin: 0;
-  position: absolute;
+  position: static;
+  max-width: 100%;
   bottom: 0;
   left: 0px;
 `;
