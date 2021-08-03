7520170a2d83fbd9dd5147a7282267610dd4a72c
AidaFig <aidafigueiredo8@gmail.com>
2021-08-03 14:00:33 -0300

    feat: TabBar exported

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Card } from "./Card";
 export { Button } from "./ButtonComponent";
 export { SplashScreen } from "./SplashScreen";
 export { Layout } from "./LayoutComponent";
+export { TabBar } from "./TabBar";
