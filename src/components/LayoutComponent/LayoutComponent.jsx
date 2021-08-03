8b2331f619e2a9bbc910e8d71e125bf46b1bdf2c
AidaFig <aidafigueiredo8@gmail.com>
2021-08-03 14:30:56 -0300

    feat: child prop added

--- src/components/LayoutComponent/LayoutComponent.jsx
+++ src/components/LayoutComponent/LayoutComponent.jsx
@@ -1,9 +1,10 @@
 import * as S from "./LayoutStyled";
 
-export const Layout = ({ hasTabBar = false }) => {
+export const Layout = ({ hasTabBar = false, children }) => {
   return (
     <>
       <S.ImageTop src="/assets/svg/detalhe topo tela.svg" />
+      { children }
       {!hasTabBar && <S.ImageBottom src="/assets/svg/detalhe baixo tela.svg" />}
     </>
   );
