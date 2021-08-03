ad074bb76c9300f70ca23550766189785a49397d
AidaFig <aidafigueiredo8@gmail.com>
2021-08-02 21:17:07 -0300

    feat: creating Layout component

---
+++ src/components/LayoutComponent/LayoutComponent.jsx
@@ -0,0 +1,11 @@
+// import { SplashScreen } from "../SplashScreen";
+import * as S from "./LayoutStyled";
+
+export const Layout = ({ hasTabBar = false }) => {
+  return (
+    <>
+      <S.ImageTop src="/assets/svg/detalhe topo tela.svg" />
+      {!hasTabBar && <S.ImageBottom src="/assets/svg/detalhe baixo tela.svg" />}
+    </>
+  );
+};
