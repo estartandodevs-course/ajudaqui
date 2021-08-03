1066b7b0388576504f10e7063437267952492c11
AidaFig <aidafigueiredo8@gmail.com>
2021-08-03 14:18:44 -0300

    feat: adding click event

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -1,22 +1,22 @@
 import * as S from "./TabBarStyled";
 
-export const TabBar = () => {
+export const TabBar = ({ onClick }) => {
   return (
     <S.TabBar>
       <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon home.svg" />
+        <S.ImageIcon src="/assets/svg/icon home.svg" onClick={onClick} />
         <S.DescriptionIcon>Início</S.DescriptionIcon>
       </S.IconContent>
       <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon ajuda.svg" />
+        <S.ImageIcon src="/assets/svg/icon ajuda.svg" onClick={onClick} />
         <S.DescriptionIcon>Ajuda</S.DescriptionIcon>
       </S.IconContent>
       <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon historico.svg" />
+        <S.ImageIcon src="/assets/svg/icon historico.svg" onClick={onClick} />
         <S.DescriptionIcon>Histórico</S.DescriptionIcon>
       </S.IconContent>
       <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon perfil.svg" />
+        <S.ImageIcon src="/assets/svg/icon perfil.svg" onClick={onClick} />
         <S.DescriptionIcon>Perfil</S.DescriptionIcon>
       </S.IconContent>
     </S.TabBar>
