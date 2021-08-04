ae8f8e012d1196a2b0c460fbcbc047093a8979b4
AidaFig <aidafigueiredo8@gmail.com>
2021-08-04 16:38:23 -0300

    feat: refactoring tabBar code

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -1,24 +1,58 @@
+import React, { useState } from "react";
+// import { useHistory } from "react-router-dom";
 import * as S from "./TabBarStyled";
 
-export const TabBar = ({ onClick }) => {
+const TabBarLinks = [
+  {
+    key: "home",
+    route: "/",
+    title: "Home",
+    icon: "/assets/svg/icon home.svg",
+  },
+  {
+    key: "help",
+    route: "/help",
+    title: "Ajuda",
+    icon: "/assets/svg/icon ajuda.svg",
+  },
+  {
+    key: "historic",
+    route: "/historic",
+    title: "Histórico",
+    icon: "/assets/svg/icon historico.svg",
+  },
+  {
+    key: "profile",
+    route: "/profile",
+    title: "Perfil",
+    icon: "/assets/svg/icon perfil.svg",
+  },
+];
+export const TabBar = () => {
+  const [currentRoute, setCurrentRoute] = useState("");
+
+  // const history = useHistory();
+
+  const handleClick = (tabBarLink) => {
+    setCurrentRoute(tabBarLink.key);
+    // history.push(tabBarLink.route);
+  };
+
   return (
     <S.TabBar>
-      <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon home.svg" onClick={onClick} />
-        <S.DescriptionIcon>Início</S.DescriptionIcon>
-      </S.IconContent>
-      <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon ajuda.svg" onClick={onClick} />
-        <S.DescriptionIcon>Ajuda</S.DescriptionIcon>
-      </S.IconContent>
-      <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon historico.svg" onClick={onClick} />
-        <S.DescriptionIcon>Histórico</S.DescriptionIcon>
-      </S.IconContent>
-      <S.IconContent>
-        <S.ImageIcon src="/assets/svg/icon perfil.svg" onClick={onClick} />
-        <S.DescriptionIcon>Perfil</S.DescriptionIcon>
-      </S.IconContent>
+      {TabBarLinks.map((tabBarLink) => (
+        <S.Ball
+          $isActive={tabBarLink.key === currentRoute}
+          key={tabBarLink.key}
+        />
+      ))}
+
+      {TabBarLinks.map((tabBarLink) => (
+        <S.IconContent key={tabBarLink.key} onClick={() => handleClick(tabBarLink)}>
+          <S.ImageIcon src={tabBarLink.icon} />
+          <S.DescriptionIcon>{tabBarLink.title}</S.DescriptionIcon>
+        </S.IconContent>
+      ))}
     </S.TabBar>
   );
 };
