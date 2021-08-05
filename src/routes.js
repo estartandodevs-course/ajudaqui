4310a623a0ee70c678d3dbe431b5142ec3df700a
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-04 22:40:46 -0300

    chore: create a route register

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Home } from "./pages";
+import { Home, Register } from "./pages";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );
