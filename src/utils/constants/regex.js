e2132a34a5d0a980dcdbc27a1ceb355947692b35
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-05 22:35:58 -0300

    feat: create a regex consts

---
+++ src/utils/constants/regex.js
@@ -0,0 +1,7 @@
+export const phoneRegExp = /^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/;
+
+export const cpfRegExp = /^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$/;
+
+export const cnpjRegExp = /^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}-[0-9]{2}$/;
+
+export const cepRegExp = /^[0-9]{5}-[0-9]{3}$/;
