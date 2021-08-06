ad1113d9af3bb3bf68ddc341b5e8771a7b42e85a
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-06 11:10:58 -0300

    chore: stylize tabs

--- src/pages/Register/Registrations/RegistrationFormStyled.js
+++ src/pages/Register/Registrations/RegistrationFormStyled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import { Tabs } from "antd";
 import { Typography } from "../../../components";
 
+const { TabPane } = Tabs;
 
 export const ContainerRegistrations = styled.div`
   width: 100%;
@@ -26,7 +28,6 @@ export const TitleRegistrations = styled(Typography).attrs({
   color: ${(props) => props.theme.palette.colors.primary};
 `;
 
-
 export const DescriptionRegistrations = styled(Typography).attrs({
   variant: "body1",
 })`
@@ -37,8 +38,32 @@ export const DescriptionRegistrations = styled(Typography).attrs({
 export const Back = styled.img`
   display: flex;
   margin-bottom: 12px;
+  cursor: pointer;
 `;
 
 export const ContentBack = styled.div`
   width: 100%;
 `;
+
+export const TabsRegister = styled(Tabs)`
+  .ant-tabs-tab{
+    padding: 0;
+    margin: 5px;
+    color: ${(props) => props.theme.palette.colors.secondaryVariant}; 
+  }
+  
+  .ant-tabs-nav::before {
+    border: none;
+  }
+
+  .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn{
+    font-family: ${(props) => props.theme.typography.body1.fontFamily};
+    color: ${(props) => props.theme.palette.colors.primary}
+  } 
+  
+  .ant-tabs-ink-bar {
+    border-bottom: 2px solid ${(props) => props.theme.palette.colors.purple};
+  }
+`;
+
+export const TabsChoiced = styled(TabPane)``;
