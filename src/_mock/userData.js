7dc57073842338047deffb6cad18084051db4a73
jonatapinto <jonatastdb_jp@hotmail.com>
2021-08-05 22:28:30 -0300

    feat: creating mock of user overview.

---
+++ src/_mock/userData.js
@@ -0,0 +1,10 @@
+export const userData = {
+  name: "Tereza",
+  img: "https://www.fsp.usp.br/nupens/wp-content/uploads/2020/09/terezacampello.jpg",
+  location: {
+    uf: "rj",
+    city: "rio de janeiro",
+  },
+  grade: 4,
+};
+
