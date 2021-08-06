8f3f8fb580210b2722ab34b73acc2a742692ba1e
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-05 22:37:43 -0300

    chore: update routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Home, Register } from "./pages";
+import { Home, Register, RegistrationForm } from "./pages";
 
 export default function Routes() {
   return (
@@ -8,6 +8,7 @@ export default function Routes() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/register/with-phone" component={RegistrationForm} />
       </Switch>
     </BrowserRouter>
   );
