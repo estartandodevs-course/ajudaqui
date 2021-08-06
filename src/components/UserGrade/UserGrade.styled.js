9315246a80bca29142814f8d897a07eb15f4ed96
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-06 15:07:36 -0300

    feat: styling and adjusting the UserGrade component images

--- src/components/UserGrade/UserGrade.styled.js
+++ src/components/UserGrade/UserGrade.styled.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const StarIcon = styled.img`
-  width:28.6px;
-  height: 25px;
-`;
+ width: 28.6px;
+ height: 25px;
+ `;
+
 export const ContainerStar = styled.div`
-display: flex;
-justify-content: center;
-width: 150px;
-height: 25px;
-`;
+ display: flex;
+ justify-content: center;
+ width: 150px;
+ height: 25px;
+ `;
+
