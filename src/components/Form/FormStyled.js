c35247e0ddc078fb4a5d438423d5f19486576d6d
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-05 22:36:49 -0300

    chore: update styles

--- src/components/Form/FormStyled.js
+++ src/components/Form/FormStyled.js
@@ -3,5 +3,7 @@ import { Form } from "formik";
 
 export const ContainerForm = styled(Form)`
   width: 100%;
-  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
