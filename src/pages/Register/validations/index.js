db47199b45ae6328eb95bdf7f6ff16ad83a7ec46
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-06 11:06:48 -0300

    refactor: validations with mask

--- src/pages/Register/validations/index.js
+++ src/pages/Register/validations/index.js
@@ -1,14 +1,20 @@
 import * as yup from "yup";
-import { phoneRegExp } from "../../../utils/constants/regex";
 
-export const validationRegister = yup.object().shape({
+export const validationRegisterWithPhone = yup.object().shape({
   name: yup.string().required("Necessário informar nome completo"),
   phone1: yup.string()
-    .required("Necessário informar número de celular")
-    .matches(phoneRegExp, "Esse celular não é valido!"),
+    .min(13, "Telefone inválido")
+    .max(15, "Telefone inválido")
+    .required("Telefone é obrigatório"),
   phone2: yup.string().oneOf([yup.ref("phone1"), null], "Entre com o mesmo número de celular"),
   password1: yup.string().min(6, "É obrigatório ter no mínimo 6 caracteres!").required("Entre com uma senha"),
   password2: yup.string().min(6, "É obrigatório ter no mínimo 6 caracteres!").oneOf([yup.ref("password1"), null], "Entre com a mesma senha"),
-  email1: yup.string().email().required("Necessário informar e-mail"),
+});
 
+export const validationRegisterWithEmail = yup.object().shape({
+  name: yup.string().required("Necessário informar nome completo"),
+  email1: yup.string().email().required("Necessário informar e-mail"),
+  email2: yup.string().email().oneOf([yup.ref("email1"), null], "Entre com o mesmo email!"),
+  password1: yup.string().min(6, "É obrigatório ter no mínimo 6 caracteres!").required("Entre com uma senha"),
+  password2: yup.string().min(6, "É obrigatório ter no mínimo 6 caracteres!").oneOf([yup.ref("password1"), null], "Entre com a mesma senha"),
 });
