08022dee725d61632426e322740080f4ac05acec
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-06 11:12:01 -0300

    feat: update registrationform

--- src/pages/Register/Registrations/RegistrationForm.jsx
+++ src/pages/Register/Registrations/RegistrationForm.jsx
@@ -1,13 +1,14 @@
 import { useHistory } from "react-router-dom";
-import { FormRegister } from "../forms";
 import { Layout } from "../../../components";
+import { RegisterWithEmail, RegisterWithPhone } from "../forms";
+import "antd/dist/antd.css";
 import * as S from "./RegistrationFormStyled";
 
-export const RegistrationForm = () => {
+export const RegistrationForm = ({ ...restProps }) => {
   const { goBack } = useHistory();
   return (
     <Layout>
-      <S.ContainerRegistrations>
+      <S.ContainerRegistrations {...restProps}>
         <S.ContentBack>
           <S.Back src="/assets/svg/back.svg" alt="back" onClick={() => goBack()} />
         </S.ContentBack>
@@ -18,8 +19,15 @@ export const RegistrationForm = () => {
           <S.DescriptionRegistrations>
             Escolha como deseja fazer seu cadastro
           </S.DescriptionRegistrations>
+          <S.TabsRegister defaultActiveKey="1">
+            <S.TabsChoiced tab="Cadastrar celular" key="1">
+              <RegisterWithPhone />
+            </S.TabsChoiced>
+            <S.TabsChoiced tab="Cadastrar e-mail" key="2">
+              <RegisterWithEmail />
+            </S.TabsChoiced>
+          </S.TabsRegister>
         </S.ContentTitle>
-        <FormRegister />
       </S.ContainerRegistrations>
     </Layout>
   );
