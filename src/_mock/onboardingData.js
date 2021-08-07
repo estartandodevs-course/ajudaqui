25058f7fab49d16794eb09353b9f3d8bd88d2246
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-07 15:02:03 -0300

    feat: creating a mock onboarding data

---
+++ src/_mock/onboardingData.js
@@ -0,0 +1,31 @@
+export const onboardingData = [
+  {
+    id: 1,
+    close: "/assets/svg/close.svg",
+    image: "/assets/svg/on boarding1.svg",
+    title: "Facilite seu dia-a-dia",
+    description: "Encontre vários voluntários com avaliações e torne seu dia mais fácil sem pagar nada por isso",
+  },
+  {
+    id: 2,
+    close: "/assets/svg/close.svg",
+    image: "/assets/svg/on boarding2.svg",
+    title: "Peça ajuda sem sair de casa",
+    description: "Você ou alguém da sua família podem pedir ajuda, oferecemos uma lista de atividades e pequenos reparos que podem ser solicitados",
+  },
+  {
+    id: 3,
+    close: "/assets/svg/close.svg",
+    image: "/assets/svg/on boarding3.svg",
+    title: "Divirta-se",
+    description: "Encontre pessoas dispostas a jogar, dançar, conversar",
+  },
+  {
+    id: 4,
+    close: "/assets/svg/close.svg",
+    image: "/assets/svg/on boarding4.svg",
+    title: "Não quer escrever?",
+    description: "Não tem problema! Você pode pedir ajuda gravando áudio e um voluntário ouvirá seu chamado",
+    showButton: true,
+  },
+];
