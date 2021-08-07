872c8567201abfdd9acd26b1f377799bc2125b2c
AidaFig <aidafigueiredo8@gmail.com>
2021-08-07 17:21:49 -0300

    feat: creating the Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,41 @@
-import { Layout, Input, Form } from "../../components";
-
+import { useHistory } from "react-router-dom";
+import { Layout, Button } from "../../components";
+import { FormLogin } from "./forms";
+import * as S from "./style";
 
 export const Login = () => {
+  const { goBack } = useHistory();
   return (
-    <Layout>
-      <Form>
-        <Input type="text" name="name" label="Nome completo" placeholder="Nome completo" />
-      </Form>
-    </Layout>
+    <>
+      <Layout>
+        <S.ContainerLogin>
+          <S.ContentBack>
+            <S.Back src="/assets/svg/back.svg" alt="back" onClick={() => goBack()} />
+          </S.ContentBack>
+          <S.ContentTitle>
+            <S.LoginTitle>
+              Que bom ver você aqui!
+            </S.LoginTitle>
+            <S.LoginDescription>
+              Faça login e você estará pronto para ajudar
+            </S.LoginDescription>
+          </S.ContentTitle>
+          <FormLogin />
+          <S.ForgotPassword to="/">
+            Esqueci a senha
+          </S.ForgotPassword>
+          <S.ButtonContainer>
+            <Button type="submit">Entrar</Button>
+          </S.ButtonContainer>
+          <S.DoRegister to="/register">
+            Não tem cadastro?
+          </S.DoRegister>
+          <Button width="210px" height="31px" background="#7C7C7C">
+            <S.IconButton src="/assets/svg/icon google.svg" alt="google" />
+            Entrar com o gmail
+          </Button>
+        </S.ContainerLogin>
+      </Layout>
+    </>
   );
 };
