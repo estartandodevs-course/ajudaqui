c8264fcf070940f1296bf0d87cfe48fcd5878d82
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-07 19:42:03 -0300

    chore: creating a mock options tag

---
+++ src/_mock/optionsTagData.js
@@ -0,0 +1,54 @@
+export const optionsTagData = [
+  {
+    id: 1,
+    option: "Leitura",
+  },
+  {
+    id: 2,
+    option: "Jogar cartas",
+  },
+  {
+    id: 3,
+    option: "Orar",
+  },
+  {
+    id: 4,
+    option: "Livros de Pintar",
+  },
+  {
+    id: 5,
+    option: "Lavar Louça",
+  },
+  {
+    id: 6,
+    option: "Jardinagem",
+  },
+  {
+    id: 7,
+    option: "Trocar Gás",
+  },
+  {
+    id: 8,
+    option: "Jogos de tabuleiros",
+  },
+  {
+    id: 9,
+    option: "Conversar",
+  },
+  {
+    id: 10,
+    option: "Caminhar",
+  },
+  {
+    id: 11,
+    option: "Trocar Lâmpada",
+  },
+  {
+    id: 12,
+    option: "Fazer Compras",
+  },
+  {
+    id: 13,
+    option: "Cozinhar",
+  },
+];
