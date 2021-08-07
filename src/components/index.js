c169f732c76edc4a1d90b2fb7485b0bf4383caee
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-07 17:20:22 -0300

    feat: creating a textarea component

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,4 @@ export { Form } from "./Form";
 export { Input } from "./Input";
 export { SelectProfileType } from "./SelectProfileType";
 export { UserGrade } from "./UserGrade";
+export { TextArea } from "./TextArea";
