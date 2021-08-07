cfbe007c77eff6526b3abbe894362c5206118c7b
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-07 19:42:16 -0300

    feat: WIP

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
+import { optionsTagData } from "../../_mock/optionsTagData";
 import * as S from "./TagStyled";
 
-export const Tag = ({ children }) => {
+export const Tag = ({ onChange, initialState }) => {
+  const [selectedTag, setSelectedTag] = useState(initialState);
+
+  useEffect(() => {
+    if (selectedTag && onChange) {
+      onChange(selectedTag);
+    }
+  }, [selectedTag]);
+
   return (
-    <S.TagContainer>
-      <S.TagContent>
-        {children}
-      </S.TagContent>
-    </S.TagContainer>
+    <>
+      {optionsTagData.map((items) => {
+        return (
+          <S.TagContainer
+            key={items.id}
+            selectedTag={selectedTag === items.id}
+            onClick={() => {
+              setSelectedTag(items.option);
+            }}
+          >
+            <S.TagContent>
+              {items.option}
+            </S.TagContent>
+          </S.TagContainer>
+        );
+      })}
+    </>
   );
 };
