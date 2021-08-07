3440bc184209b655818dec70c5c3187b34fa75ff
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-07 15:01:12 -0300

    feat: creating a onboarding page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { Home } from "./Home";
 export { Register } from "./Register";
 export { RegistrationForm } from "./Register/Registrations/RegistrationForm";
+export { Onboarding } from "./Onboarding";
