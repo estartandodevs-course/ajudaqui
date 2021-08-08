a61bf9b15924e6c76f7324958b59f4794831e20d
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-07 23:12:16 -0300

    chore: estimated time tag data

--- src/_mock/optionsTagData.js
+++ src/_mock/optionsTagData.js
@@ -2,53 +2,66 @@ export const optionsTagData = [
   {
     id: 1,
     option: "Leitura",
+    estimatedTime: 30,
   },
   {
     id: 2,
     option: "Jogar cartas",
+    estimatedTime: 30,
   },
   {
     id: 3,
     option: "Orar",
+    estimatedTime: 30,
   },
   {
     id: 4,
     option: "Livros de Pintar",
+    estimatedTime: 30,
   },
   {
     id: 5,
     option: "Lavar Louça",
+    estimatedTime: 30,
   },
   {
     id: 6,
     option: "Jardinagem",
+    estimatedTime: 30,
   },
   {
     id: 7,
     option: "Trocar Gás",
+    estimatedTime: 30,
   },
   {
     id: 8,
     option: "Jogos de tabuleiros",
+    estimatedTime: 30,
   },
   {
     id: 9,
     option: "Conversar",
+    estimatedTime: 30,
   },
   {
     id: 10,
     option: "Caminhar",
+    estimatedTime: 30,
   },
   {
     id: 11,
     option: "Trocar Lâmpada",
+    estimatedTime: 30,
   },
   {
     id: 12,
     option: "Fazer Compras",
+    estimatedTime: 30,
   },
   {
     id: 13,
     option: "Cozinhar",
+    estimatedTime: 30,
   },
 ];
