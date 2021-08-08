406d822c0d0d1d886350889e6cfd9ea7512df92c
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-07 23:12:36 -0300

    refactor: replace props in tag

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,34 +1,14 @@
-import { useEffect, useState } from "react";
-import { optionsTagData } from "../../_mock/optionsTagData";
 import * as S from "./TagStyled";
 
-export const Tag = ({ onChange }) => {
-  const [selectedTag, setSelectedTag] = useState([]);
-
-
-  useEffect(() => {
-    if (selectedTag && onChange) {
-      onChange(selectedTag);
-    }
-  }, [selectedTag]);
-
+export const Tag = ({ isActive, children, ...restProps }) => {
   return (
-    <>
-      {optionsTagData.map(({ id, option }) => {
-        return (
-          <S.TagContainer
-            key={id}
-            className={selectedTag[id - 1] === option && "active"}
-            onClick={() => {
-              setSelectedTag([...selectedTag, option]);
-            }}
-          >
-            <S.TagContent>
-              {option}
-            </S.TagContent>
-          </S.TagContainer>
-        );
-      })}
-    </>
+    <S.TagContainer
+      isActive={isActive}
+      {...restProps}
+    >
+      <S.TagContent>
+        {children}
+      </S.TagContent>
+    </S.TagContainer>
   );
 };
