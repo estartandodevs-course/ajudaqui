f9f7e126744206010059608c10baa70ad6124f49
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-09 16:29:40 -0300

    chore: passing sizes by props

--- src/components/UserGrade/UserGrade.styled.js
+++ src/components/UserGrade/UserGrade.styled.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 export const StarIcon = styled.img`
- width: 28.6px;
- height: 25px;
+ width: ${(props) => (props.width ? props.width : "28.6px")};
+ height:${(props) => (props.height ? props.height : "25px")};
+ margin-left: 4px;
  `;
 
 export const ContainerStar = styled.div`
  display: flex;
  justify-content: center;
- width: 150px;
  height: 25px;
+ margin-left: 6px;
  `;
 
