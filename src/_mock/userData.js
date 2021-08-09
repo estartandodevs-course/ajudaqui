b710ea04c1f81fd7a9e4fe4a77d0d04252b4b394
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-09 16:31:15 -0300

    chore: correcting errors in texts

--- src/_mock/userData.js
+++ src/_mock/userData.js
@@ -1,10 +1,10 @@
 export const userData = {
   name: "Tereza",
   img: "https://www.fsp.usp.br/nupens/wp-content/uploads/2020/09/terezacampello.jpg",
+  idade: 75,
   location: {
-    uf: "rj",
-    city: "rio de janeiro",
+    uf: "RJ",
+    city: "Rio de janeiro",
   },
   grade: 4,
 };
-
