58ddbfa2bccc4218c3d9d970d56c858e9d72c3ad
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-09 21:07:02 -0300

    feat: form and validations

--- src/pages/UserInformation/forms/FormPersonalInfos.jsx
+++ src/pages/UserInformation/forms/FormPersonalInfos.jsx
@@ -7,15 +7,24 @@ import * as S from "./FormStyled";
 export const PersonalInfos = () => {
   return (
     <>
-      <Form>
+      <Form
+        initialValues={{
+          name: "",
+          birthday: "",
+          gender: "",
+          address: "",
+          phone: "",
+          prefference: "",
+        }}
+      >
         <Input type="text" name="name" label="Nome completo" placeholder="Nome completo" />
         <Input type="date" name="birthday" label="Data de nascimento" placeholder="01/01/1990" width="168px" />
         <Input type="text" name="gender" label="Gênero" placeholder="Feminino" />
         <Input type="texte" name="address" label="Endereço" placeholder="Rua 00 Casa 00" />
         <Input type="tel" name="phone" label="Telefone" placeholder="(00) 0123-4567" />
-        <Form.InputSelect name="prefference" options={optionsPreference} />
+        <Form.InputSelect label="Preferência de ajuda" name="prefference" options={optionsPreference} />
         <S.ButtonContainer>
-          <Button width="100px">Editar</Button>
+          <Button width="100px" type="submit">Editar</Button>
         </S.ButtonContainer>
       </Form>
     </>
