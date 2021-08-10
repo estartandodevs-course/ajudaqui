0fcb55c7fcc296b83f5409e8bcbff4a9b540663d
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-10 08:46:00 -0300

    feat: validate contacts

--- src/pages/UserInformation/validations/index.js
+++ src/pages/UserInformation/validations/index.js
@@ -1,11 +1,18 @@
 import * as yup from "yup";
 
-
 export const validationPersonalInformation = yup.object().shape({
   name: yup.string(),
   birthday: yup.date(),
   gender: yup.string(),
   address: yup.string(),
+  phone: yup.string().min(13, "Telefone inválido")
+    .max(15, "Telefone inválido"),
+});
+
+
+export const validationContacts = yup.object().shape({
+  name: yup.string(),
+  kinship: yup.string(),
   phone: yup.string().min(13, "Telefone inválido")
     .max(15, "Telefone inválido"),
 });
