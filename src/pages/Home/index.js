481a0e0dfd7d417a641ead1b021df59c0183cf57
AidaFig <aidafigueiredo8@gmail.com>
2021-08-11 12:12:18 -0300

    feat: showing off BackNavigation component on the browser;

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Layout, SplashScreen } from "../../components";
+import { BackNavigation, SplashScreen } from "../../components";
+// import { Layout, SplashScreen } from "../../components";
 
 export const Home = () => {
   return (
-    <Layout>
+    // <Layout>
+    //   <SplashScreen />
+    // </Layout>
+    <>
+      <BackNavigation />
       <SplashScreen />
-    </Layout>
+    </>
   );
 };
