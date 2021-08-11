055a516c335a5eae756171e7cbd94b284db2121f
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-11 09:21:06 -0300

    feat: make ask for help and dynamic tag

--- src/_mock/index.js
+++ src/_mock/index.js
@@ -1,3 +1,4 @@
 export { userData } from "./userData";
 export { onboardingData } from "./onboardingData";
 export { optionsPreference } from "./preferenceData";
+export { optionsTagData } from "./optionsTagData";
