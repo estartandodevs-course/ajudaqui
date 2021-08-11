8e37cdf61ef3e0c942a861d8dc8b39353a5a4f0e
AidaFig <aidafigueiredo8@gmail.com>
2021-08-11 15:34:53 -0300

    feat: adding conditional to the component

--- src/components/BackNavigation/BackNavigation.jsx
+++ src/components/BackNavigation/BackNavigation.jsx
@@ -1,12 +1,13 @@
 import { useHistory } from "react-router-dom";
+import { Layout } from "../LayoutComponent";
 import * as S from "./BackNavigationStyled";
 
-export const BackNavigation = () => {
+export const BackNavigation = ({ hasNoArrow = false }) => {
   const { goBack } = useHistory();
   return (
     <S.Container>
-      <S.ImageTop src="/assets/svg/detalhe topo tela.svg" />
-      <S.Back src="/assets/svg/back.svg" onClick={() => goBack()} />
+      { !hasNoArrow && <S.ImageTop src="/assets/svg/detalhe topo tela.svg" /> }
+      { !hasNoArrow ? <S.Back src="/assets/svg/back.svg" onClick={() => goBack()} /> : <Layout /> }
     </S.Container>
   );
 };
