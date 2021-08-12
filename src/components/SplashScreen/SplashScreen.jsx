8f61f5589e6fb605c8e9e1589cdf74938af3b34d
AidaFig <aidafigueiredo8@gmail.com>
2021-08-12 09:35:52 -0300

    feat: adding a validation prop

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,23 +1,17 @@
 import { useHistory } from "react-router-dom";
-import { useWidthScreen } from "../../utils/hooks/useWidthScreen";
 import { Button } from "../ButtonComponent/ButtonStyled";
-import { Layout } from "../LayoutComponent";
 import * as S from "./SplashScreenStyled";
 
-export const SplashScreen = () => {
+export const SplashScreen = ({ hasButton = true }) => {
   const { push } = useHistory();
-  const [widthScreen] = useWidthScreen();
-  const showNavigation = widthScreen <= 1200;
   return (
-    <Layout hasArrow={false} showNavigation={showNavigation}>
-      <S.Container>
-        <S.Title>
-          Precisando de ajuda?
-        </S.Title>
-        <S.Subtitle>Encontre um voluntário(a)</S.Subtitle>
-        <S.ImageCenter src="/assets/svg/Logo completa.svg" />
-        <Button style={{ zIndex: "1" }} onClick={() => push("register")}>Começar</Button>
-      </S.Container>
-    </Layout>
+    <S.Container>
+      <S.Title>
+        Precisando de ajuda?
+      </S.Title>
+      <S.Subtitle>Encontre um voluntário(a)</S.Subtitle>
+      <S.ImageCenter src="/assets/svg/Logo completa.svg" />
+      {hasButton && <Button style={{ zIndex: "1" }} onClick={() => push("register")}>Começar</Button>}
+    </S.Container>
   );
 };
