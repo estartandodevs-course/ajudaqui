d22b85d77f451d1d462f4b27c3c692a9f0b81e89
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-12 08:48:49 -0300

    feat: implement logic to infinite scroll in carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,10 +1,45 @@
+import { useRef } from "react";
 import * as S from "./CarouselStyled";
 
 export const Carousel = ({
-  height, display, margin, children, ...restProps
+  height,
+  display,
+  margin,
+  children,
+  onNextEnd,
+  infinity,
+  autoPlaySpeed,
+  ...restProps
 }) => {
+  let resetTimeout;
+  const carouselRef = useRef(null);
+
+  const handleInfinity = ({ index }, reference) => {
+    clearTimeout(resetTimeout);
+    if (index + 1 === children?.length) {
+      resetTimeout = setTimeout(() => {
+        reference.current.goTo(0);
+      }, autoPlaySpeed);
+    }
+  };
+
   return (
-    <S.ContainerCarousel height={height} margin={margin} display={display} {...restProps}>
+    <S.ContainerCarousel
+      ref={carouselRef}
+      height={height}
+      margin={margin}
+      display={display}
+      autoPlaySpeed={autoPlaySpeed}
+      onNextEnd={(nextItem, index) => {
+        if (onNextEnd) {
+          onNextEnd(nextItem, index);
+        }
+        if (infinity) {
+          handleInfinity(nextItem, carouselRef);
+        }
+      }}
+      {...restProps}
+    >
       {children}
     </S.ContainerCarousel>
   );
