1baf641e8f6ec4a4370cd003be9c8342977bd815
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-12 15:46:17 -0300

    feat: adding structure to the HomeScreenOfTheVoluntary screen

---
+++ src/pages/Home/HomeScreenOfTheVoluntary/HomeScreenOfTheVoluntary.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./HomeScreenOfTheVoluntaryStyled";
+import { Layout } from "../../../components";
+
+export const HomeScreenOfTheVoluntary = () => {
+  return (
+    <Layout hasTabBar>
+      <S.ContainerPage>
+        Home do voluntario
+      </S.ContainerPage>
+    </Layout>
+  );
+};
---
+++ src/pages/Home/HomeScreenOfTheVoluntary/HomeScreenOfTheVoluntaryStyled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ContainerPage = styled.div`
+width: 100%;
+padding: 0 20px;
+`;
+
