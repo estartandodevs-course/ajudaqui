16ea55cb96210e5791d2d941b8a60b65dae57d51
AidaFig <aidafigueiredo8@gmail.com>
2021-08-12 09:36:36 -0300

    feat: adding the desktop style in the styled component

--- src/pages/Register/RegisterStyled.js
+++ src/pages/Register/RegisterStyled.js
@@ -3,6 +3,20 @@ import { Link } from "react-router-dom";
 import { Typography } from "../../components";
 
 export const ContainerRegister = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  height: 100vh;
+  @media(min-width: 576px) {
+    justify-items: center;
+  }
+  @media(min-width: 1200px) {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+  }
+`;
+
+export const RegisterForm = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -12,10 +26,37 @@ export const ContainerRegister = styled.div`
   @media(max-width: 360px){
     padding: 62px 12px;
   }
+  @media(min-width: 1200px){
+  justify-content: center;
+  align-items: center;
+  }
+`;
+
+export const RegisterAside = styled.aside`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(
+  rgba(254, 253, 255, 0.4),
+  rgba(216, 205, 238, 0.4),
+  rgba(94, 56, 168, 0.4));
+
+  @media(max-width: 1200px) {
+    display: none;
+  }
 `;
 
 export const ContentTitle = styled.div`
   width: 100%;
+
+  @media(min-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const ContentChoice = styled.div`
@@ -32,7 +73,12 @@ export const TitleRegister = styled(Typography).attrs({
 
 export const ChoiceProfile = styled(Typography).attrs({
   variant: "h2",
-})``;
+})`
+@media(min-width: 1200px) {
+  width: 100%;
+  text-align: center;
+}
+`;
 
 export const NextStep = styled(Link)`
   display: flex;
@@ -44,6 +90,10 @@ export const NextStep = styled(Link)`
   color: ${(props) => props.theme.palette.colors.text};
   font-family: ${(props) => props.theme.typography.body1.fontFamily};
   font-size: ${(props) => props.theme.typography.body1.fontSizeText};
+
+  @media(min-width:1200px) {
+    display: none;
+  }
 `;
 
 export const IconNext = styled.img``;
