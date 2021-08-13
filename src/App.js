485d47ad1fcdd750475c1f928962783815a31801
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-13 10:35:37 -0300

    chore: organizing imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { AuthProvider } from "./contexts/Auth";
-import Routes from "./routes";
 import { GlobalStyle } from "./styles/global";
 import { theme } from "./styles/themes";
+import Routes from "./routes";
 
 export default function App() {
   return (
