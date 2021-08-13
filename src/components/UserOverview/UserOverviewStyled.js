384aa246d085bdf6bf7b496d7938433b43985c5e
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-13 20:18:54 -0300

    feat: update services

--- src/components/UserOverview/UserOverviewStyled.js
+++ src/components/UserOverview/UserOverviewStyled.js
@@ -1,40 +1,38 @@
 import styled from "styled-components";
 import { Typography } from "../Typography";
 
-
 export const ContainerProfile = styled.div`
-display: flex;
+  display: flex;
 `;
 
 export const ContainerData = styled.div`
-display: flex;
-flex-direction: column;
-margin-top: 70px;
-margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 70px;
+  margin-left: 10px;
 `;
+
 export const ContainerGrade = styled.div`
-display: flex;
-justify-content: space-between;
+  display: flex;
 `;
 
 export const ImgProfile = styled.img`
-width: 74.31px;
-height: 75px;
-border: 2px solid #5E38A8;
-border-radius: 100px;
-object-fit: cover;
-margin-top: 84px;
-/* margin-left: 20px; */
+  width: 74.31px;
+  height: 75px;
+  border: 2px solid #5e38a8;
+  border-radius: 100px;
+  object-fit: cover;
+  margin-top: 84px;
 `;
+
 export const TitleProfile = styled(Typography).attrs({
   variant: "h1",
   color: "#4E3681",
 })`
-margin-bottom: 2px;
-  `;
+  margin-bottom: 2px;
+`;
 export const Paragraph = styled(Typography).attrs({
   variant: "body1",
 })`
-margin-bottom: 2px;
-  `;
-
+  margin-bottom: 2px;
+`;
