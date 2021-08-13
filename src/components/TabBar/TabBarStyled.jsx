2d903c246ba60dc112f7130f677d19564c2e2b59
AidaFig <aidafigueiredo8@gmail.com>
2021-08-13 15:56:01 -0300

    feat: creating the desktop TabBAr style

--- src/components/TabBar/TabBarStyled.jsx
+++ src/components/TabBar/TabBarStyled.jsx
@@ -2,6 +2,20 @@ import styled from "styled-components";
 import { theme } from "../../styles/themes";
 import { Typography } from "..";
 
+export const Wrapper = styled.div`
+width: 100%;
+
+@media(min-width: 1200px) {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 95px;
+  height: 100vh;
+  background-image: ${(props) => `${props.theme.palette.colors.gradientBackground}`};
+  border: none;
+  }
+`;
+
 export const TabBar = styled.div`
   width: 100%;
   display: grid;
@@ -12,6 +26,16 @@ export const TabBar = styled.div`
   border-image: ${(props) => `${props.theme.palette.colors.gradientBackground} 1`};
   align-self: flex-end;
   height: 64px;
+
+   @media(min-width: 1200px) {
+    width: 95px;
+    height: 300px;
+    display:grid;
+    grid-template-columns: repeat(1, 4fr);
+    align-items: center;
+    margin-left: 10px;
+    border: none;
+  }
 `;
 
 export const IconContent = styled.div`
@@ -19,9 +43,23 @@ export const IconContent = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 25%;
+  width: 100%;
+  height: 100%;
   margin: 8px 0;
   cursor: pointer;
+
+  @media(min-width: 1200px) {
+    display:flex;
+    align-items: flex-start;
+    width: 80px;
+    height: 65px;
+    margin-left: auto;
+    border: none;
+    border-top-left-radius: 48%;
+    border-bottom-left-radius: 48%;
+    background-color: ${(props) => (props.$containerPosition ? "white" : `${props} => ${props.theme.palette.colors.gradientBackground}`)};
+
+  }
 `;
 
 export const Ball = styled.div`
@@ -41,12 +79,21 @@ export const Ball = styled.div`
   background: ${(props) => props.theme.palette.colors.gradientBackground};
   border-radius: 100%;
   transition: .6s ease-in-out;
+
+  @media(min-width: 1200px) {
+    display: none;
+  }
 `;
 
 export const ImageIcon = styled.img`
   margin-bottom: 4px;
   max-width: 26px;
   max-height: 26px;
+
+  @media(min-width: 1200px) {
+    margin-left: 15px;
+    filter: ${(props) => (props.$containerPosition ? `${props} => ${props.theme.palette.colors.gradientBackground} 1` : "brightness(100)")};
+  }
 `;
 
 export const DescriptionIcon = styled(Typography).attrs({
@@ -54,5 +101,10 @@ export const DescriptionIcon = styled(Typography).attrs({
   color: theme.palette.colors.primary,
 })`
   font-size: ${(props) => props.theme.typography.body1?.fontSizeSubtitles};
+
+  @media(min-width: 1200px) {
+    display: none;
+  }
+
 `;
 
