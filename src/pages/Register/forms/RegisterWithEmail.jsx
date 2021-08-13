db579651d3f10dc2576f5994399ed138b6c127ef
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-13 20:45:51 -0300

    feat: implement register with email service

--- src/pages/Register/forms/RegisterWithEmail.jsx
+++ src/pages/Register/forms/RegisterWithEmail.jsx
@@ -4,11 +4,10 @@ import { validationRegisterWithEmail } from "../validations";
 import * as S from "./RegisterStyled";
 
 const RegisterWithEmail = () => {
-  const { profileType } = useAuth();
+  const { profileType, authIsLoading, registerEmail } = useAuth();
   return (
     <Form
       initialValues={{
-        typeProfile: profileType,
         name: "",
         email1: "",
         email2: "",
@@ -16,12 +15,15 @@ const RegisterWithEmail = () => {
         password2: "",
       }}
       validationSchema={validationRegisterWithEmail}
+      onSubmit={async ({ name, email1, password1 }) => {
+        registerEmail({ name, email: email1, password: password1 }, profileType);
+      }}
     >
-      <Input type="text" name="name" label="Nome completo" placeholder="Nome completo" />
-      <Input type="text" name="email1" label="Seu e-mail" placeholder="nome@example.com" />
-      <Input type="text" name="email2" label="Confirme seu e-mail" placeholder="nome@example.com" />
-      <Input type="password" name="password1" label="Sua senha" placeholder="6 ou mais caracteres" />
-      <Input type="password" name="password2" label="Confirme sua senha" placeholder="Digite a mesma senha de cima" />
+      <Input type="text" name="name" label="Nome completo" placeholder="Nome completo" disabled={authIsLoading} />
+      <Input type="text" name="email1" label="Seu e-mail" placeholder="nome@example.com" disabled={authIsLoading} />
+      <Input type="text" name="email2" label="Confirme seu e-mail" placeholder="nome@example.com" disabled={authIsLoading} />
+      <Input type="password" name="password1" label="Sua senha" placeholder="6 ou mais caracteres" disabled={authIsLoading} />
+      <Input type="password" name="password2" label="Confirme sua senha" placeholder="Digite a mesma senha de cima" disabled={authIsLoading} />
       <S.PositionButton>
         <Button type="submit">CADASTRAR</Button>
       </S.PositionButton>
