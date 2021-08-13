9c506e15184d42cff37fc2a12287964bc30f4cc9
AidaFig <aidafigueiredo8@gmail.com>
2021-08-13 17:25:05 -0300

    feat: exporting tabBarLinks to the mock

---
+++ src/_mock/tabBar.js
@@ -0,0 +1,34 @@
+export const TabBarLinks = [
+  {
+    key: "home",
+    route: "/",
+    title: "Home",
+    icon: "/assets/svg/icon home.svg",
+    dotPosition: 1,
+    containerPosition: 0,
+  },
+  {
+    key: "help",
+    route: "/help",
+    title: "Ajuda",
+    icon: "/assets/svg/icon ajuda.svg",
+    dotPosition: 3,
+    containerPosition: 1,
+  },
+  {
+    key: "historic",
+    route: "/historic",
+    title: "Histórico",
+    icon: "/assets/svg/icon historico.svg",
+    dotPosition: 5,
+    containerPosition: 2,
+  },
+  {
+    key: "profile",
+    route: "/profile",
+    title: "Perfil",
+    icon: "/assets/svg/icon perfil.svg",
+    dotPosition: 7,
+    containerPosition: 3,
+  },
+];
