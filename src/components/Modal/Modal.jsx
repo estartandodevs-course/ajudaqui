58ce9a5adf0529e217282f171d5ec94fbf075f05
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-16 20:01:48 -0300

    fix: resolving errors in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
 import * as S from "./ModalStyled";
 
-export const Modal = ({ children }) => {
+export const Modal = ({ children, alignItems }) => {
   return ReactDOM.createPortal(
-    <S.Modal>
+    <S.Modal alignItems={alignItems}>
       {children}
     </S.Modal>,
     document.getElementById("modal-root"),
--- src/components/WelcomeModalCards/ModalCardsStyled.jsx
+++ src/components/WelcomeModalCards/ModalCardsStyled.jsx
@@ -3,49 +3,50 @@ import { theme } from "../../styles/themes";
 import { Typography } from "../Typography";
 
 export const CardContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-width: 312px;
-height: 358px;
-border-radius: 10px;
-background-color: ${(props) => props.theme.palette.colors.white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 312px;
+  height: 358px;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.palette.colors.white};
+  margin-bottom: 20px;
 `;
 export const Close = styled.img`
-width: 12.01px;
-height: 12.01px;
-margin: 16px 16px 0 auto;
-filter: brightness(0%);
+  width: 12.01px;
+  height: 12.01px;
+  margin: 16px 16px 0 auto;
+  filter: brightness(0%);
 `;
 
 export const Title = styled(Typography).attrs({
   variant: "h2",
   color: theme.palette.colors.primary,
 })`
-font-size: ${(props) => props.theme.typography.h2?.fontSize};
-margin-top: 13px;
+  font-size: ${(props) => props.theme.typography.h2?.fontSize};
+  margin-top: 13px;
 `;
 
 export const LogoImage = styled.img`
-width: 117px;
-height: 117px;
-margin-top: 24px;
+  width: 117px;
+  height: 117px;
+  margin-top: 24px;
 `;
 
 export const Description = styled(Typography).attrs({
   variant: "body1",
 })`
-font-size: ${(props) => props.theme.typography.h2?.fontSizeText};
-width: 242px;
-margin-top: 24px;
+  font-size: ${(props) => props.theme.typography.h2?.fontSizeText};
+  width: 242px;
+  margin-top: 24px;
 `;
 
 export const ArrowDown = styled.img`
-margin-right: 16px;
-margin-left: auto;
+  margin-right: 16px;
+  margin-left: auto;
 `;
 
 export const Span = styled.span`
-color: ${(props) => props.theme.palette.colors.emergency};
-font-weight: 700;
+  color: ${(props) => props.theme.palette.colors.emergency};
+  font-weight: 700;
 `;
