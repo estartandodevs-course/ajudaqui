26dc62a2d94b37664379676b31ee70a5a648a09c
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-16 18:52:40 -0300

    refactor: replace prop type person to profile type

--- src/components/UserOverview/UserOverview.jsx
+++ src/components/UserOverview/UserOverview.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import * as S from "./UserOverviewStyled";
 import { UserGrade } from "../UserGrade";
+import { useAuth } from "../../contexts";
 
-export const UserOverview = ({ userData, typePerson }) => {
+export const UserOverview = ({ userData }) => {
   const {
     name, photoURL, location, birthday, grade,
   } = userData;
+
+  const { profileType } = useAuth();
+
   return (
     <>
-      {typePerson === "Elderly" ? (
+      {!profileType === "elderly" ? (
 
         <S.ContainerProfile>
           <S.ImgProfile src={photoURL} alt={name} />
