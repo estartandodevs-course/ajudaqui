6f00a8a2679a245a9513bc20958174be001fa0b2
AidaFig <aidafigueiredo8@gmail.com>
2021-08-16 09:42:25 -0300

    feat: HelpRequestCard mock created ;

---
+++ src/_mock/HelpRequestCard.js
@@ -0,0 +1,8 @@
+export const elderlyInfos = {
+  name: "Maria Rita",
+  task: "Trocar gás",
+  distance: "3km",
+  orderTime: "10min",
+  photo: "/assets/images/oldman-1.jpg",
+  action: "Ajudando",
+};
