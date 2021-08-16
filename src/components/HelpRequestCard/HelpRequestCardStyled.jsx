845b00a45b65ba7359e6f540e52e56a0b4869051
AidaFig <aidafigueiredo8@gmail.com>
2021-08-16 09:44:19 -0300

    feat: creating the HelpRequestedCard component style

--- src/components/HelpRequestCard/HelpRequestCardStyled.jsx
+++ src/components/HelpRequestCard/HelpRequestCardStyled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "../../styles/themes";
 import { Typography } from "..";
 
@@ -9,12 +9,52 @@ justify-content: center;
 padding: 20px 24px;
 `;
 
+const variants = {
+  default: css`
+  border: 1px solid ${(props) => `${props.theme.palette.colors.purple} `};
+  `,
+
+  emergency: css`
+  border: 1px solid ${(props) => `${props.theme.palette.colors.emergency} `};
+  `,
+
+  gradient: css`
+  border: 1px solid blue;
+  `,
+
+  primaryVariant: css`
+  border: 1px solid ${(props) => `${props.theme.palette.colors.primaryVariant} `};
+`,
+};
+
 export const Card = styled.div`
 width: 312px;
 height: 175px;
-border: 1px solid purple;
 border-radius: 5px 5px 9px 9px;
+${(props) => props.variant && variants[props.variant]}
 `;
+
+const backgrounds = {
+  default: css`
+  background-color: ${(props) => `${props.theme.palette.colors.secondaryVariant} `};
+  border-top-color: ${(props) => `${props.theme.palette.colors.purple} `};
+  `,
+
+  emergency: css`
+  background-color: ${(props) => `${props.theme.palette.colors.emergency} `};
+  border-top-color: ${(props) => `${props.theme.palette.colors.emergency} `};
+  `,
+
+  gradient: css`
+  background-image: ${(props) => `${props.theme.palette.colors.gradientBackground} `};
+  border-top-color: ${(props) => `${props.theme.palette.colors.gradientBackground} `};
+  `,
+
+  primaryVariant: css`
+  background-color: ${(props) => `${props.theme.palette.colors.primaryVariant} `};
+  border-top-color: ${(props) => `${props.theme.palette.colors.primaryVariant} `};
+  `,
+};
 
 export const UserAction = styled.div`
 width: 100%;
@@ -22,18 +62,19 @@ height: 24%;
 border-top: 1px solid;
 border-radius: 0 0 9px 9px;
 border-image: ${(props) => `${props.theme.palette.colors.gradientBackground} 1`};
-background-color: ${(props) => `${props.theme.palette.colors.secondaryVariant} `};
+${(props) => props.variant && backgrounds[props.variant]}
 `;
 
 export const ActionDescription = styled(Typography).attrs({
   variant: "body1",
-  color: theme.palette.colors.primary,
 })`
 display: flex;
 justify-content: center;
 align-items: center;
+text-transform: uppercase;
 width: 100%;
 height: 100%;
+color: ${(props) => props.color || props.theme.palette.colors.white};
 `;
 
 export const UserInfos = styled.div`
@@ -46,9 +87,21 @@ height: 76%;
 export const Request = styled.div`
 display: flex;
 flex-direction: column;
-justify-content: center;
+gap: 20px;
 border: none;
 flex: 2;
+`;
+
+export const NameTask = styled.div`
+display: flex;
+flex-direction: column;
+gap: 10px;
+`;
+
+export const DistanceTimeContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 10px;
 `;
 
 export const UserName = styled(Typography).attrs({
@@ -67,12 +120,12 @@ font-size: ${(props) => props.theme.typography.body1?.fontSizeText};
 `;
 
 export const Distance = styled(Typography).attrs({
-  variante: "body",
+  variante: "body1",
 })`
 text-transform: uppercase;
 font-weight: 700;
 margin-bottom: 0;
-font-size: ${(props) => props.theme.typography.body1?.fontSizeText};
+font-size: ${(props) => props.theme.typography.body1?.fontSizeSubtitles};
 `;
 
 export const TaskTime = styled(Typography).attrs({
@@ -80,21 +133,22 @@ export const TaskTime = styled(Typography).attrs({
   color: theme.palette.colors.light.gray,
 })`
 text-transform: uppercase;
-font-size: ${(props) => props.theme.typography.body1?.fontSizeText};
+font-size: ${(props) => props.theme.typography.body1?.fontSizeSubtitles};
 margin-bottom: 0;
 `;
 
 export const UserImage = styled.div`
 display: flex;
 justify-content: center;
-align-items: flex-start;
+align-items: center;
 flex: 1;
 border: none;
 `;
 
 export const Image = styled.img`
-width: 90px;
-height: 90px;
+width: 85px;
+height: 85px;
 border-radius: 100%;
 object-fit: cover;
 `;
+
