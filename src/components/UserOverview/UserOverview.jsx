28f706e40f5fd4d385cc26414078614eeb48255d
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-16 17:50:49 -0300

    feat: structuring conditional

--- src/components/UserOverview/UserOverview.jsx
+++ src/components/UserOverview/UserOverview.jsx
@@ -2,30 +2,35 @@ import React from "react";
 import * as S from "./UserOverviewStyled";
 import { UserGrade } from "../UserGrade";
 
-export const UserOverview = ({ userData }) => {
+export const UserOverview = ({ userData, typePerson }) => {
   const {
     name, photoURL, location, birthday, grade,
   } = userData;
   return (
-    <S.ContainerProfile>
-      <S.ImgProfile src={photoURL} alt={name} />
-      <S.ContainerData>
-        <S.TitleProfile>
-          {`Olá, ${name}`}
-        </S.TitleProfile>
-        <S.Paragraph>
-          {birthday
+    <>
+      {typePerson === "idoso" ? (
+
+        <S.ContainerProfile>
+          <S.ImgProfile src={photoURL} alt={name} />
+          <S.ContainerData>
+            <S.TitleProfile>
+              {`Olá, ${name}`}
+            </S.TitleProfile>
+            <S.Paragraph>
+              {birthday
         && (
         <>
           {`${birthday} anos - ${location.city}`}
         </>
         )}
-        </S.Paragraph>
-        <S.ContainerGrade>
-          <S.Paragraph>Sua nota</S.Paragraph>
-          <UserGrade grade={grade} width="18.88px" height="16px" />
-        </S.ContainerGrade>
-      </S.ContainerData>
-    </S.ContainerProfile>
+            </S.Paragraph>
+            <S.ContainerGrade>
+              <S.Paragraph>Sua nota</S.Paragraph>
+              <UserGrade grade={grade} width="18.88px" height="16px" />
+            </S.ContainerGrade>
+          </S.ContainerData>
+        </S.ContainerProfile>
+      ) : null}
+    </>
   );
 };
