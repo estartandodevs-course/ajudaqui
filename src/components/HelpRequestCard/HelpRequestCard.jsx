6a3262e2a90f05466192a4b85505fdc8ce4d5767
AidaFig <aidafigueiredo8@gmail.com>
2021-08-16 09:43:44 -0300

    feat: creating the HelpRequestedCard component

--- src/components/HelpRequestCard/HelpRequestCard.jsx
+++ src/components/HelpRequestCard/HelpRequestCard.jsx
@@ -2,33 +2,40 @@
 import React from "react";
 import * as S from "./HelpRequestCardStyled";
 
-export const HelpRequestCard = () => {
+export const HelpRequestCard = ({ elderlyInfos, variant, color }) => {
+  const {
+    name, task, distance, orderTime, photo, action,
+  } = elderlyInfos;
   return (
     <>
       <S.CardWrapper>
-        <S.Card>
+        <S.Card variant={variant}>
           <S.UserInfos>
             <S.Request>
-              <S.UserName>
-                Maria Rita
-              </S.UserName>
-              <S.RequestedTask>
-                Trocar gás
-              </S.RequestedTask>
-              <S.Distance>
-                Há 3 km
-              </S.Distance>
-              <S.TaskTime>
-                Pedido feito há 10min
-              </S.TaskTime>
+              <S.NameTask>
+                <S.UserName>
+                  {name}
+                </S.UserName>
+                <S.RequestedTask>
+                  {task}
+                </S.RequestedTask>
+              </S.NameTask>
+              <S.DistanceTimeContainer>
+                <S.Distance>
+                  {`Há ${distance}`}
+                </S.Distance>
+                <S.TaskTime>
+                  {`Pedido feito há ${orderTime}`}
+                </S.TaskTime>
+              </S.DistanceTimeContainer>
             </S.Request>
             <S.UserImage>
-              <S.Image src="/assets/images/oldman-1.jpg" />
+              <S.Image src={photo} />
             </S.UserImage>
           </S.UserInfos>
-          <S.UserAction>
-            <S.ActionDescription>
-              AJUDANDO
+          <S.UserAction variant={variant}>
+            <S.ActionDescription color={color}>
+              {action}
             </S.ActionDescription>
           </S.UserAction>
         </S.Card>
