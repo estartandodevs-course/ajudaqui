d1b6e7e2c423af50d19f375d9aa35cd08194378e
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-16 21:38:52 -0300

    feat: make a screen evaluation and stylize

---
+++ src/pages/ScreenEvaluation/ScreenEvaluation.jsx
@@ -0,0 +1,61 @@
+
+import { useState } from "react";
+import {
+  Button,
+  Form, Input, Layout, ServiceEvaluation, UserGrade,
+} from "../../components";
+import { serviceEvaluationData } from "../../_mock";
+import * as S from "./ScreenEvaluationStyled";
+
+export const ScreenEvaluation = () => {
+  const [evaluation, setEvaluation] = useState(0);
+  return (
+    <Layout hasTabBar>
+      <S.ContainerScreenEvaluation>
+        <S.ContentTitle>
+          <S.TitleHelp>
+            Ajuda encerrada
+          </S.TitleHelp>
+          <S.DescriptionPersonHelp>
+            Por favor, avalie a pessoa que lhe ajudou
+          </S.DescriptionPersonHelp>
+          <S.UserHelped src="/assets/svg/sara.svg" alt="helped" />
+        </S.ContentTitle>
+        <S.ContentEvaluation>
+          <S.NamePersonHelp>
+            Sara Patrícia
+          </S.NamePersonHelp>
+          <UserGrade grade={evaluation} />
+          <S.ContainerEvaluation>
+            {serviceEvaluationData.map((item) => {
+              return (
+                <ServiceEvaluation
+                  key={item.id}
+                  src={item.icon}
+                  alt={item.description}
+                  click={() => setEvaluation(item.id)}
+                >
+                  {item.description}
+                </ServiceEvaluation>
+              );
+            }) }
+          </S.ContainerEvaluation>
+        </S.ContentEvaluation>
+        <Form
+          initialValues={{
+            comment: "",
+            grade: evaluation,
+          }}
+          enableReinitialize
+        >
+          <Input type="text" name="comment" label="Deixar comentário (opcional)" />
+          <S.PositionButton>
+            <Button type="submit">
+              enviar
+            </Button>
+          </S.PositionButton>
+        </Form>
+      </S.ContainerScreenEvaluation>
+    </Layout>
+  );
+};
---
+++ src/pages/ScreenEvaluation/ScreenEvaluationStyled.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { Typography } from "../../components";
+
+export const ContainerScreenEvaluation = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 14px 24px;
+`;
+
+export const ContentTitle = styled.div`
+  display: flex;  
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const ContainerEvaluation = styled.div`
+  display: flex;
+  width: min-content;
+  gap: 20px;
+`;
+
+export const TitleHelp = styled(Typography).attrs({
+  variant: "h2",
+})`
+  color: ${(props) => props.theme.palette.colors.primaryVariant};
+`;
+
+export const DescriptionPersonHelp = styled(Typography).attrs({
+  variant: "body1",
+})`
+  font-weight: 700;
+  max-width: 190px;
+  text-align: center;
+  margin: 0 auto;
+`;
+
+export const UserHelped = styled.img`
+  width: 85px;
+  height: 85px;
+  border-radius: 9999px;
+  margin-bottom: 16px;
+`;
+
+export const ContentEvaluation = styled.div`
+  display: flex;  
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 28px;
+`;
+
+export const NamePersonHelp = styled(Typography).attrs({
+  variant: "body1",
+})`
+`;
+
+export const PositionButton = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%auto;
+`;
