843918114c15824f5402f4b6bbf8af5987dfadbf
AidaFig <aidafigueiredo8@gmail.com>
2021-08-17 16:20:32 -0300

    feat: exporting VolunterrHomeScreen directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,3 +6,4 @@ export { Onboarding } from "./Onboarding";
 export { UserInformation } from "./UserInformation";
 export { AskForHelp } from "./AskForHelp";
 export { ScreenEvaluation } from "./ScreenEvaluation";
+export { VolunteerHome } from "./VolunteerHomeScreen";
