7a9a35bbfedaca44feee63ed5221dfc7e09ce174
AidaFig <aidafigueiredo8@gmail.com>
2021-08-17 16:23:20 -0300

    feat: changing file name

--- src/_mock/index.js
+++ src/_mock/index.js
@@ -3,5 +3,5 @@ export { onboardingData, onboardingDataDesktop } from "./onboardingData";
 export { optionsPreference } from "./preferenceData";
 export { optionsTagData } from "./optionsTagData";
 export { TabBarLinks } from "./tabBar";
-export { elderlyInfos } from "./HelpRequestCard";
+export { elderlyInfos } from "./HelpRequestCardData";
 export { serviceEvaluationData } from "./serviceEvaluationData";
