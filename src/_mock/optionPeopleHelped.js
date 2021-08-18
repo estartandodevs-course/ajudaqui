c4d0e3c4e60eb1628e905b7f1bf4acad82f89d66
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-17 23:22:11 -0300

    feat: creating a new mock opitionPeopleHelped with photos of seniors who were helped by the volunteer

---
+++ src/_mock/optionPeopleHelped.js
@@ -0,0 +1,28 @@
+
+export const optionPeopleHelped = [
+  {
+    photo: "/assets/images/oldman-1.jpg",
+  },
+  {
+    photo: "/assets/images/oldman-2.jpg",
+  },
+  {
+    photo: "/assets/images/oldman-3.jpg",
+  },
+  {
+    photo: "/assets/images/oldman-4.jpg",
+  },
+  {
+    photo: "/assets/images/oldman-5.jpg",
+  },
+  {
+    photo: "/assets/images/oldman-6.jpg",
+  },
+  {
+    photo: "/assets/images/oldman-7.jpg",
+  },
+  {
+    photo: "/assets/images/oldman-8.jpg",
+  },
+];
+
