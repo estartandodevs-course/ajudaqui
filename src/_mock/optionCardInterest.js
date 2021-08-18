b3f5d0c0515646908d25e814518d501357b24301
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-17 23:20:12 -0300

    feat: creating a new mock opitionCardInterest for the tags of interest

---
+++ src/_mock/optionCardInterest.js
@@ -0,0 +1,27 @@
+export const optionCardInterest = [
+  {
+    id: 1,
+    option: "Leitura",
+    estimatedTime: 30,
+  },
+  {
+    id: 2,
+    option: "Jogar cartas",
+    estimatedTime: 30,
+  },
+  {
+    id: 3,
+    option: "Orar",
+    estimatedTime: 30,
+  },
+  {
+    id: 4,
+    option: "Livros de pintar",
+    estimatedTime: 30,
+  },
+  {
+    id: 5,
+    option: "Artesanato",
+    estimatedTime: 30,
+  },
+];
