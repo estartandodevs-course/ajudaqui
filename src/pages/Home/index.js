597a189ff8fd98739d62c2dba639f837922e8697
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-17 23:06:01 -0300

    feat: Adding the HomeScreenOfTheVoluntary screen to the home index

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,13 +3,13 @@ import { SplashScreen } from "../../components";
 import { useAuth } from "../../contexts";
 import { PROFILES_TYPES } from "../../utils/constants";
 import { HomeScreenOfTheElderly } from "./HomeScreenOfTheElderly/HomeScreenOfTheElderly";
-import { HomeScreenOfTheVoluntary } from "./HomeScreenOfTheVoluntary/HomeScreenOfTheVoluntary";
+import { HomeScreenOfTheVoluntary } from "./HomeScreenOfTheVoluntary";
 
 export const Home = () => {
   const { profileType } = useAuth();
   const homeScreenByProfile = {
     [PROFILES_TYPES.ELDERLY]: () => <HomeScreenOfTheElderly />,
-    [PROFILES_TYPES.VOLUNTARY]: () => <HomeScreenOfTheVoluntary />,
+    [PROFILES_TYPES.VOLUNTARY]: () => <HomeScreenOfTheVoluntary horas={30} atividades={10} />,
   };
 
   return (
