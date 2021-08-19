a41c57ab4a2ae91e6338fc1930a35bef1c849e76
AidaFig <aidafigueiredo8@gmail.com>
2021-08-19 20:49:36 -0300

    feat: elements to the desktop screen added

--- src/pages/AskForHelp/AskForHelp.jsx
+++ src/pages/AskForHelp/AskForHelp.jsx
@@ -5,8 +5,12 @@ import {
 import { useAuth, useStore } from "../../contexts";
 import { optionsTagData } from "../../_mock";
 import * as S from "./AskForHelpStyled";
+import { useWidthScreen } from "../../utils/hooks/useWidthScreen";
 
 export const AskForHelp = ({ ...restProps }) => {
+  const [widthScreen] = useWidthScreen();
+
+  const showNavigation = widthScreen <= 1200;
   const [isActive, setIsActive] = useState(null);
   const [selectedOptionHelp, setSelectedOptionHelp] = useState({});
   const { handleCreateOrder } = useStore();
@@ -23,37 +27,42 @@ export const AskForHelp = ({ ...restProps }) => {
     });
   };
   return (
-    <Layout hasTabBar>
-      <S.ContainerAskForHelp {...restProps}>
-        <Form
-          initialValues={{
-            help: "",
-          }}
-          onSubmit={handleSubmit}
-        >
-          <S.Text>
-            Selecione alguma das atividades abaixo ou escreva seu pedido.
-          </S.Text>
-          <S.ContainerTag>
-            {optionsTagData?.map(({ id, option }) => (
-              <Tag
-                key={id}
-                isActive={isActive === id}
-                onClick={() => {
-                  setIsActive(id);
-                  setSelectedOptionHelp({ id, option });
-                }}
-              >
-                {option}
-              </Tag>
-            ))}
-          </S.ContainerTag>
-          <Input type="text" name="help" label="Precisando de ajuda com outra coisa?" placeholder="Clique aqui para escrever" />
-          <S.PositionButton>
-            <Button type="submit">Enviar Pedido</Button>
-          </S.PositionButton>
-        </Form>
-      </S.ContainerAskForHelp>
+    <Layout hasTabBar showNavigation={showNavigation}>
+      <S.ContainerDesktop>
+        <S.ContainerAskForHelp {...restProps}>
+          <Form
+            initialValues={{
+              help: "",
+            }}
+            onSubmit={handleSubmit}
+          >
+            <S.Text>
+              Selecione alguma das atividades abaixo ou escreva seu pedido.
+            </S.Text>
+            <S.ContainerTag>
+              {optionsTagData?.map(({ id, option }) => (
+                <Tag
+                  key={id}
+                  isActive={isActive === id}
+                  onClick={() => {
+                    setIsActive(id);
+                    setSelectedOptionHelp({ id, option });
+                  }}
+                >
+                  {option}
+                </Tag>
+              ))}
+            </S.ContainerTag>
+            <Input type="text" name="help" label="Precisando de ajuda com outra coisa?" placeholder="Clique aqui para escrever" />
+            <S.PositionButton>
+              <Button type="submit">Enviar Pedido</Button>
+            </S.PositionButton>
+          </Form>
+        </S.ContainerAskForHelp>
+        <S.Aside>
+          <S.AsideImage src="/assets/svg/arte idoso desktop.svg" />
+        </S.Aside>
+      </S.ContainerDesktop>
     </Layout>
   );
 };
