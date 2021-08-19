218b1b4892459b7751f86a3fe0251de362d31ac9
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-19 00:19:31 -0300

    feat: adding store provider

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import { GlobalStyle } from "./styles/global";
 import { theme } from "./styles/themes";
 import Routes from "./routes";
 import { ModalProvider } from "./contexts/Modal";
+import { StoreProvider } from "./contexts/Store";
 
 export default function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <AuthProvider>
-          <ModalProvider>
-            <GlobalStyle />
-            <Routes />
-          </ModalProvider>
+          <StoreProvider>
+            <ModalProvider>
+              <GlobalStyle />
+              <Routes />
+            </ModalProvider>
+          </StoreProvider>
         </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
