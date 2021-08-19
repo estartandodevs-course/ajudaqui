d7b3d55adcccf67b45b7a74695729fdb0efa7a37
AidaFig <aidafigueiredo8@gmail.com>
2021-08-19 17:06:34 -0300

    feat: elements to the desktop added

--- src/pages/Home/HomeScreenOfTheElderly/HomeScreenOfTheElderly.jsx
+++ src/pages/Home/HomeScreenOfTheElderly/HomeScreenOfTheElderly.jsx
@@ -13,70 +13,89 @@ export const HomeScreenOfTheElderly = () => {
   const showNavigation = widthScreen < 1200;
   return (
     <Layout hasTabBar showNavigation={showNavigation}>
-      <S.ContainerPage>
-        <S.ContainerUserOverview>
-          <UserOverview userData={user} />
-        </S.ContainerUserOverview>
-        <S.ContainerOne
-          borderTop="1px solid #D8CDEE"
-          borderBottom="1px solid #D8CDEE"
-        >
-          <S.Paragraph>Como você prefere pedir ajuda?</S.Paragraph>
-          <S.ContainerCards>
-            <Card
-              src="/assets/svg/icon mic.svg"
-              color="#fff"
-              fontSize="14px"
-            >
-              POR ÁUDIO
-            </Card>
-            <Card
-              src="/assets/svg/icon texto.svg"
-              color="#fff"
-              fontSize="14px"
-            >
-              POR TEXTO
-            </Card>
-          </S.ContainerCards>
-        </S.ContainerOne>
-        <S.ContainerOne borderBottom="1px solid #D8CDEE">
-          <S.Paragraph1>Informações Pessoais</S.Paragraph1>
-          <S.ContainerCards>
-            <Card
-              variant="secondary"
-              src="/assets/svg/icon saude.svg"
-              color="#fff"
-              fontSize="14px"
-            >
-              DADOS DE SAÚDE
-            </Card>
-            <Card
-              variant="secondary"
-              src="/assets/svg/icon contato.svg"
-              color="#fff"
-              fontSize="14px"
-            >
-              AGENDA DE CONTATOS
-            </Card>
-          </S.ContainerCards>
-        </S.ContainerOne>
-        <S.ContainerTwo>
-          <S.ContainerTexts>
-            <S.TextInformation>Possui uma emergência de saúde?</S.TextInformation>
-          </S.ContainerTexts>
-          <S.Paragraph1>
-            Clique abaixo e seus familiares e voluntários serão avisados que
-            você precisa de socorro
-          </S.Paragraph1>
-          <S.PositionButton>
-            <Button
-              background={(props) => props.theme.palette.colors.emergency}
-            >
-              EMERGÊNCIA
-            </Button>
-          </S.PositionButton>
-        </S.ContainerTwo>
-      </S.ContainerPage>
+      <S.ContainerPageAside>
+        <S.ContainerPage>
+          <S.ContainerUserOverview>
+            <UserOverview userData={user} />
+          </S.ContainerUserOverview>
+          <S.ContainerOne
+            borderTop="1px solid #D8CDEE"
+            borderBottom="1px solid #D8CDEE"
+          >
+            <S.Paragraph>Como você prefere pedir ajuda?</S.Paragraph>
+            <S.ContainerCards>
+              <Card
+                src="/assets/svg/icon mic.svg"
+                color="#fff"
+                fontSize="14px"
+              >
+                POR ÁUDIO
+              </Card>
+              <Card
+                src="/assets/svg/icon texto.svg"
+                color="#fff"
+                fontSize="14px"
+              >
+                POR TEXTO
+              </Card>
+            </S.ContainerCards>
+          </S.ContainerOne>
+          <S.ContainerOne borderBottom="1px solid #D8CDEE">
+            <S.Paragraph1>Informações Pessoais</S.Paragraph1>
+            <S.ContainerCards>
+              <Card
+                variant="secondary"
+                src="/assets/svg/icon saude.svg"
+                color="#fff"
+                fontSize="14px"
+              >
+                DADOS DE SAÚDE
+              </Card>
+              <Card
+                variant="secondary"
+                src="/assets/svg/icon contato.svg"
+                color="#fff"
+                fontSize="14px"
+              >
+                AGENDA DE CONTATOS
+              </Card>
+            </S.ContainerCards>
+          </S.ContainerOne>
+          <S.ContainerTwo>
+            <S.ContainerTexts>
+              <S.TextInformation>Possui uma emergência de saúde?</S.TextInformation>
+            </S.ContainerTexts>
+            <S.Paragraph2>
+              Clique abaixo e seus familiares e voluntários serão avisados que
+              você precisa de socorro
+            </S.Paragraph2>
+            <S.PositionButton>
+              <Button
+                background={(props) => props.theme.palette.colors.emergency}
+              >
+                EMERGÊNCIA
+              </Button>
+            </S.PositionButton>
+          </S.ContainerTwo>
+        </S.ContainerPage>
+        <S.HelpEmergency>
+          <S.HelpImage src="/assets/svg/emergencia.svg" />
+          <S.TextContainer>
+            <S.Text>
+              Possui uma emergência de saúde?
+            </S.Text>
+            <S.Subtitle>
+              Clique abaixo e seus familiares e voluntários serão avisados que
+              você precisa de socorro
+            </S.Subtitle>
+          </S.TextContainer>
+          <Button
+            background={(props) => props.theme.palette.colors.emergency}
+          >
+            EMERGÊNCIA
+          </Button>
+        </S.HelpEmergency>
+      </S.ContainerPageAside>
     </Layout>
   );
 };
