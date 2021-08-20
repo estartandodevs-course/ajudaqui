32ebbfe42103e0ba85cf442480ab27251844d09f
AidaFig <aidafigueiredo8@gmail.com>
2021-08-19 22:03:51 -0300

    feat: desktop style done

--- src/pages/ScreenEvaluation/ScreenEvaluationStyled.js
+++ src/pages/ScreenEvaluation/ScreenEvaluationStyled.js
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 import { Typography } from "../../components";
 
-export const ContainerDesktop = styled.div``;
-export const Aside = styled.div``;
-export const AsideImage = styled.div``;
+export const ContainerDesktop = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  height: 100vh;
+  @media (min-width: 576px) {
+    justify-items: center;
+  }
+  @media (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
+export const Aside = styled.div`
+width: 100%;
+position: relative;
+background-color: ${(props) => props.theme.palette.colors.light.grayish};
+
+@media (max-width: 1200px) {
+    display: none;
+  }
+`;
+
+export const AsideImage = styled.img`
+position: absolute;
+bottom: 0;
+right: 0;
+`;
 
 
 export const ContainerScreenEvaluation = styled.div`
@@ -12,6 +38,12 @@ export const ContainerScreenEvaluation = styled.div`
   width: 100%;
   height: 100%;
   padding: 14px 24px;
+
+  @media (min-width: 1200px) {
+    width: 400px;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const ContentTitle = styled.div`
@@ -19,6 +51,7 @@ export const ContentTitle = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 16px;
+
 `;
 
 export const ContainerEvaluation = styled.div`
@@ -31,6 +64,10 @@ export const TitleHelp = styled(Typography).attrs({
   variant: "h2",
 })`
   color: ${(props) => props.theme.palette.colors.primaryVariant};
+
+  @media (min-width: 1200px) {
+  font-size: 24px;
+  }
 `;
 
 export const DescriptionPersonHelp = styled(Typography).attrs({
@@ -40,6 +77,11 @@ export const DescriptionPersonHelp = styled(Typography).attrs({
   max-width: 190px;
   text-align: center;
   margin: 0 auto;
+
+  @media (min-width: 1200px) {
+    font-size: 18px;
+    max-width: 100%;
+  }
 `;
 
 export const UserHelped = styled.img`
@@ -47,6 +89,11 @@ export const UserHelped = styled.img`
   height: 85px;
   border-radius: 9999px;
   margin-bottom: 16px;
+
+  @media (min-width: 1200px) {
+  width: 160px;
+  height: 160px;
+  }
 `;
 
 export const ContentEvaluation = styled.div`
@@ -55,15 +102,27 @@ export const ContentEvaluation = styled.div`
   align-items: center;
   gap: 8px;
   margin-bottom: 28px;
+
+  @media (min-width: 1200px) {
+  gap: 20px;
+  }
 `;
 
 export const NamePersonHelp = styled(Typography).attrs({
   variant: "body1",
 })`
+
+@media (min-width: 1200px) {
+  font-size: 18px;
+  }
 `;
 
 export const PositionButton = styled.div`
   display: flex;
   justify-content: center;
   width: 100%auto;
+
+  @media (min-width: 1200px) {
+  margin-top: 20px;
+  }
 `;
