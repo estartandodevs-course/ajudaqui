332907cb012c2eb44b2b9e8a5efaa1f8c64b99a8
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-19 22:08:55 -0300

    feat: update hook store and set promise alls

--- src/contexts/Store/hooks/useStore.js
+++ src/contexts/Store/hooks/useStore.js
@@ -3,12 +3,52 @@ import { StoreContext } from "../index";
 import { useOrderList } from "./useOrderList";
 import { useCreateOrder } from "./useCreateOrder";
 import { useUpdateOrder } from "./useUpdateOrder";
+import { MountStoreActionsTypes } from "../types";
+import { fetchElderlys } from "../../../services/elderlyService";
+import { fetchVoluntarys } from "../../../services/voluntaryService";
+import { fetchTags } from "../../../services/tagsService";
+import { fetchHelpRequests } from "../../../services/helpRequestService";
+import { useTag } from "./useTag";
 
 export const useStore = () => {
-  const { state } = useContext(StoreContext);
+  const { state, dispatch } = useContext(StoreContext);
+  const handleMount = async () => {
+    dispatch({
+      type: MountStoreActionsTypes.MOUNT_STORE_START,
 
+    });
+    try {
+      const response = await Promise.all([
+        fetchElderlys(), fetchVoluntarys(), fetchTags(), fetchHelpRequests(),
+      ]);
+
+      const payload = {
+        elderlys: response[0],
+        voluntarys: response[1],
+        tags: response[2],
+        helpRequests: response[3],
+      };
+
+      dispatch({
+        type: MountStoreActionsTypes.MOUNT_STORE_SUCESS,
+        payload,
+      });
+    } catch (error) {
+      dispatch({
+        type: MountStoreActionsTypes.MOUNT_STORE_ERROR,
+        payload: {
+          error,
+        },
+      });
+    }
+  };
 
   return {
-    ...state, ...useOrderList(), ...useCreateOrder(), ...useUpdateOrder(),
+    ...state,
+    ...useOrderList(),
+    ...useCreateOrder(),
+    ...useUpdateOrder(),
+    ...useTag(),
+    handleMount,
   };
 };
