499ba2fb8f7ea4ca438f9024c0a500175bca5767
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-23 19:58:18 -0300

    feat: update help request card

--- src/components/HelpRequestCard/HelpRequestCard.jsx
+++ src/components/HelpRequestCard/HelpRequestCard.jsx
@@ -1,40 +1,56 @@
 
 import React from "react";
-
 import { differenceInMinutes, parseISO } from "date-fns";
 import { useStore } from "../../contexts";
 import * as S from "./HelpRequestCardStyled";
+import { orderStatusName } from "../../utils/constants";
 
 export const HelpRequestCard = ({
-  variant = "default", color, colorTask, helpRequestData,
+  helpRequestData, isVoluntary,
 }) => {
   const { elderlys } = useStore();
 
   const {
-    elderly: { id: elderlyId }, order, createdAt,
+    elderly: { id: elderlyId }, order, createdAt, status,
   } = helpRequestData;
-  const elderlyProfile = elderlys.find((elderly) => elderly.id === elderlyId);
 
+  const elderlyProfile = elderlys
+    .find((elderly) => elderly.id === elderlyId);
 
   const {
     name,
     photoURL,
   } = elderlyProfile;
 
-
   const runningTime = differenceInMinutes(new Date(), parseISO(createdAt));
+
+  const verifyWaitingStatus = orderStatusName[status] === "aguardando" && !isVoluntary;
+
+  const hasEmergency = order?.key === "emergency";
+
+  const verifyOrderStatus = (hasEmergency && "emergency")
+   || (verifyWaitingStatus && "aguardando")
+   || "default";
+
+  const actionsTypes = (hasEmergency && "emergência")
+   || (verifyWaitingStatus && "aguardando")
+   || "ajudando";
 
   return (
     <>
       <S.CardWrapper>
-        <S.Card variant={variant}>
+        <S.Card
+          $variant={verifyOrderStatus}
+        >
           <S.UserInfos>
             <S.Request>
               <S.NameTask>
                 <S.UserName>
                   {name}
                 </S.UserName>
-                <S.RequestedTask colorTask={colorTask}>
+                <S.RequestedTask
+                  $colorTask={hasEmergency && "#BC1610"}
+                >
                   {order.option}
                 </S.RequestedTask>
               </S.NameTask>
@@ -48,14 +64,20 @@ export const HelpRequestCard = ({
               </S.DistanceTimeContainer>
             </S.Request>
             <S.UserImage>
-              <S.Image src={photoURL} />
+              <S.Image
+                src={photoURL}
+              />
             </S.UserImage>
           </S.UserInfos>
-          <S.UserAction variant={variant}>
-            <S.ActionDescription color={color}>
-              {/* {action} */}
-              {" "}
-              {/* { src && <img src={src} alt="next" />} */}
+          <S.UserAction
+            $variant={verifyOrderStatus}
+          >
+            <S.ActionDescription
+              color={isVoluntary && "#4e3681"}
+            >
+              {actionsTypes}
+              {!isVoluntary
+                 && <img src="assets/svg/Vector.svg" alt="next" loading="lazy" />}
             </S.ActionDescription>
           </S.UserAction>
         </S.Card>
