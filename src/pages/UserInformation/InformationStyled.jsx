eec39add39f364b71d44ac9ac88c03058cd57a7c
AidaFig <aidafigueiredo8@gmail.com>
2021-08-23 21:43:27 -0300

    creating the user information desktop

--- src/pages/UserInformation/InformationStyled.jsx
+++ src/pages/UserInformation/InformationStyled.jsx
@@ -4,6 +4,36 @@ import { Typography } from "../../components";
 
 const { TabPane } = Tabs;
 
+export const ContainerDesktop = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  @media (min-width: 576px) {
+    justify-items: center;
+  }
+  @media (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+  }
+`;
+
+export const Aside = styled.div`
+  width: 100%;
+  position: relative;
+  background-color: ${(props) => props.theme.palette.colors.light.grayish};
+
+@media (max-width: 1199px) {
+    display: none;
+  }
+`;
+
+export const AsideImage = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
 export const ContainerForm = styled.div`
   width: 100%;
   display: flex;
