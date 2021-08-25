e9135b63225ee4504cc9d770d2e4d94ee889d549
Ramon Xavier <ramon1234rxm@gmail.com>
2021-08-24 22:55:09 -0300

    conflicts

--- src/contexts/Store/index.js
+++ src/contexts/Store/index.js
@@ -1,13 +1,22 @@
-import { createContext, useReducer } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import { createContext, useEffect, useReducer } from "react";
 import { StoreReducer } from "./reducers";
 import { initialStateStoreReducer, storeContextModel } from "./models";
+import { useStorage } from "../../utils/hooks/useStorage";
 
 const StoreContext = createContext(storeContextModel);
 const { Provider } = StoreContext;
 
+
 const StoreProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(StoreReducer, initialStateStoreReducer);
+  const { setStorageData, getStorageData } = useStorage();
+
+  const [state, dispatch] = useReducer(StoreReducer, getStorageData(initialStateStoreReducer));
+
+  useEffect(() => {
+    setStorageData(state);
+  }, [state]);
+
   const notify = (content, type = "success") => {
     const TYPES = {
       success: () => toast.success(content),
