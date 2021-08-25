916d49f3126793fa8f72364cad4c29ed59642db8
AidaFig <aidafigueiredo8@gmail.com>
2021-08-25 13:16:18 -0300

    feat: creating desktop style

--- src/pages/IssueCertificate/IssueCertificateStyled.jsx
+++ src/pages/IssueCertificate/IssueCertificateStyled.jsx
@@ -1,13 +1,45 @@
 import styled from "styled-components";
 import { Typography } from "../../components";
 
+export const ContainerDesktop = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+  @media (min-width: 576px) {
+    justify-items: center;
+  }
+  @media (min-width: 1199px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+  }
+`;
+
+export const Aside = styled.div`
+  width: 100%;
+  position: relative;
+  background-color: ${(props) => props.theme.palette.colors.light.grayish};
+
+@media (max-width: 1199px) {
+    display: none;
+  }
+`;
+
+export const ImageAside = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
+
 export const ContainerPage = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
 margin-top: 20px;
-gap: 30px;
+gap: 20px;
+margin: 0 24px;
 `;
 
 export const ContainerImage = styled.div`
@@ -46,6 +78,11 @@ export const Paragraph = styled(Typography).attrs({
 })`
 width: 272px;
 text-align: center;
+
+@media (min-width: 1199px) {
+  width: 400px;
+  text-align: left;
+  }
 `;
 
 export const Paragraph2 = styled(Typography).attrs({
@@ -54,4 +91,28 @@ export const Paragraph2 = styled(Typography).attrs({
 width: 280px;
 text-align: center;
 font-size: 12px;
+
+@media (min-width: 1199px) {
+  width: 400px;
+  }
 `;
+
+export const CertificateContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 11px;
+width: 100%;
+height: 44px;
+padding: 0 15px;
+border: 1px solid #7d6d96;
+border-radius: 5px;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const CertificateContent = styled.div`
+font-size: 12px;
+`;
+export const ImageDownload = styled.img``;
+
+
