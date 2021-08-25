16734764b83fefe581a670f7dece607de3291e90
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-08-24 22:52:13 -0300

    feat: domeScreenOfTheVoluntary desktop page styling done

--- src/pages/Home/HomeScreenOfTheVoluntary/HomeScreenOfTheVoluntaryStyled.js
+++ src/pages/Home/HomeScreenOfTheVoluntary/HomeScreenOfTheVoluntaryStyled.js
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 import { Typography } from "../../../components";
 
+
+export const PagesContainer = styled.div`
+width: 100%;
+display: flex;
+`;
+export const ContainerPage = styled.div`
+  @media (min-width: 1200px){
+    width: 50%;
+    margin-left: 70px;
+    height: 100vh;
+    overflow-y: scroll;
+}
+`;
+
 export const ContainerHelpCard = styled.div`
   width: 100%;
 `;
@@ -10,18 +24,30 @@ export const ContainerTitles = styled.div`
   flex-direction: column;
   gap: 4px;
   margin: 22px 24px;
+  @media (min-width: 1200px) {
+    flex-direction: row;
+    margin-top: 50px;
+    margin-bottom: 31px;
+  }
 `;
 
 export const Title = styled(Typography).attrs({
   variant: "h2",
 })`
   color: ${(props) => props.theme.palette.colors.text};
+   @media (min-width: 1200px) {
+    color: ${(props) => props.theme.palette.colors.primaryVariant};
+  }
 `;
 
 export const Subtitle = styled(Typography).attrs({
   variant: "body1",
 })`
   color: ${(props) => props.theme.palette.colors.text};
+  @media (min-width: 1200px) {
+    color: ${(props) => props.theme.palette.colors.primaryVariant};
+    margin-top: 6px;
+  }
 `;
 
 export const HelpAvailable = styled.div`
@@ -41,4 +67,20 @@ export const LocationText = styled(Typography).attrs({
 })`
   color: ${(props) => props.theme.palette.colors.dark.gray};
   font-size: 12px;
+`;
+
+export const ContainerPageTwo = styled.div`
+  width: 50%;
+  background-color: #F9F7FB;
+  display: flex;
+  align-items: flex-end;
+@media (max-width: 1199px) {
+  display: none;
+}
+`;
+
+export const Image = styled.img`
+@media (max-width: 1199px) {
+  display: none;
+}
 `;
