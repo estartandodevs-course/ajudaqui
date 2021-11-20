b6ed6325e75af6f45c4f1c36a8b0d56e62386e9a
Ramon <ramon1234rxm@gmail.com>
2021-11-20 12:55:54 -0300

    refactor: variants button and styles

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -1,14 +1,16 @@
 import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 import * as S from './ButtonStyled';
-import { IButtonProps } from './interfaces';
+import { ButtonDefaultProps, IButtonProps } from './interfaces';
 
 const antIcon = <LoadingOutlined style={{ fontSize: 24, color: '#f9f7fd' }} spin />;
 
-export const Button = ({ isLoading, children }: IButtonProps) => (
-  <S.Button>
+export const Button = ({ isLoading, children, variant }: IButtonProps) => (
+  <S.Button variant={variant}>
     {isLoading ? (
       <Spin indicator={antIcon} />
     ) : children}
   </S.Button>
 );
+
+Button.defaultProps = ButtonDefaultProps;
--- src/components/elements/Button/interfaces/ButtonProps.tsx
+++ src/components/elements/Button/interfaces/ButtonProps.tsx
@@ -3,4 +3,9 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode,
   isLoading?: boolean,
+  variant?: string,
 }
+
+export const ButtonDefaultProps: IButtonProps = {
+  variant: 'default',
+};
